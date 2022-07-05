52b2fa573a8a037a7d7e3ccc353f6530f4252439
Knight174 <cpc4253@163.com>
2022-07-05 08:02:20 +0800

    declarative

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('root'))
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(<h1>Hello, React!</h1>)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<h1 className="header">Hello, React!</h1>)
 
-const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program.";
-h1.className = "header"
-document.getElementById("root").append(h1);
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is an imperative way to program.";
+// h1.className = "header"
+// document.getElementById("root").append(h1);
