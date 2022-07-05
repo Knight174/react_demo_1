45441a2c76dd2bceefa287de54ea7cf7fc972328
Knight174 <cpc4253@163.com>
2022-07-05 08:07:05 +0800

    JSX

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('root'))
-
-// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1 className="header">Hello, React!</h1>)
-
 // const h1 = document.createElement("h1");
 // h1.textContent = "This is an imperative way to program.";
 // h1.className = "header"
 // document.getElementById("root").append(h1);
+
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// JSX
+const element = <h1 className="header">Hello, React!</h1>
+console.log(element)
+// {
+//     "type": "h1",
+//     "key": null,
+//     "ref": null,
+//     "props": {
+//         "className": "header",
+//         "children": "Hello, React!"
+//     },
+//     "_owner": null,
+//     "_store": {}
+// }
+
+root.render(element)
