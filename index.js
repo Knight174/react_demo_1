95c7bc259400246003009ec262cfcc350f40bc0a
Knight174 <cpc4253@163.com>
2022-07-06 08:12:19 +0800

    make a custom component by function

--- index.js
+++ index.js
@@ -6,18 +6,20 @@
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const page = (
-  <div>
-    <img src="./logo192.png" alt="" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function Page() {
+  return (
+    <div>
+      <img src="./logo192.png" alt="" width="40px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
 
-root.render(page)
+root.render(Page())
