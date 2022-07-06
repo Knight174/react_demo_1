dc6b3cda42492489da4563468e801f2803339fd8
Knight174 <cpc4253@163.com>
2022-07-06 08:42:47 +0800

    split components

--- index.js
+++ index.js
@@ -6,14 +6,27 @@
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-function Page() {
+function Header () {
+  return (
+    <header>
+      <nav>
+        <img src="./logo192.png" alt="" width="40px" />
+      </nav>
+    </header>
+  )
+}
+
+function Footer () {
+  return (
+<footer>
+        <small>&copy;2022 Eric development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./logo192.png" alt="" width="40px" />
-        </nav>
-      </header>
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
@@ -22,11 +35,18 @@ function Page() {
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-      <footer>
-        <small>&copy;2022 Eric development. All rights reserved.</small>
-      </footer>
     </div>
   )
 }
 
-root.render(Page())
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+root.render(<Page />)
