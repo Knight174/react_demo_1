fd41885deab3a5dec350171c4108de7ab743d951
Knight174 <cpc4253@163.com>
2022-07-09 15:59:40 +0800

    init project by cra

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer () {
-  return (
-    <footer>
-      <small>&copy;2022 Eric development. All rights reserved.</small>
-    </footer>
-  )
-}
