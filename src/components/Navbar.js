86e0b3f9693b4aa63aa1251143f2b24174a5e87d
Knight174 <cpc4253@163.com>
2022-07-10 10:04:47 +0800

    change navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,9 @@ import reactLogo from "../assets/images/logo192.png";
 export default function Navbar() {
   return (
     <nav>
-      <div className="nav--left">
         <img src={reactLogo} alt="React Logo" className="nav--icon" />
         <h3 className="nav--logo_text">React Facts</h3>
-      </div>
-      <div className="nav--right">
         <h4 className="nav--title">React Course - Project1</h4>
-      </div>
     </nav>
   )
 }
